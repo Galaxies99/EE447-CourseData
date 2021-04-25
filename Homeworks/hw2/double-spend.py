52c5a7212a06fdc91c5a5b1b35a4e64cdee36149
Tony Fang <galaxies@vip.qq.com>
2021-04-25 14:20:15 +0800

    Update.

--- Homeworks/hw2/double-spend.py
+++ Homeworks/hw2/double-spend.py
@@ -26,26 +26,19 @@ def calc_expected_cost(k, p, c, epsilon = 0.1):
     P = []
     D = []
     x = 0
+    residual_P = 1
     while True:
         Px = comb(k + x + x + 2, x) * pow(p, k + x + 2) * pow(1 - p, x)
         if x > 0:
-            for y in range(x - 1):
+            for y in range(x):
                 Px = Px - P[y] * comb(2 * (x - y), (x - y)) * pow(p, x - y) * pow(1 - p, x - y)
         if Px == np.inf:
+            delta = residual_P * (k + x + x + 2) * c
+            E += delta
             break
+        residual_P -= Px
         P.append(Px)
         delta = Px * (k + x + x + 2) * c
-        D.append(delta)
-        E += delta
-        x += 1
-    
-    reg = LinearRegression()
-    reg.fit(np.array(range(x - 100, x)).reshape(-1, 1), D[len(D) - 100:])
-
-    while True:
-        delta = reg.predict(np.array([x]).reshape(1, -1))[0]
-        if delta < 0:
-            break
         E += delta
         x += 1
     return E 
@@ -53,6 +46,7 @@ def calc_expected_cost(k, p, c, epsilon = 0.1):
 E_value = []
 for k in tqdm(range(LIMIT)):
     E_value.append(calc_expected_cost(k, p, c))
+print(E_value)
 
 plt.scatter(range(LIMIT), E_value)
 plt.plot(range(LIMIT), [100] * LIMIT, color='r', linestyle='--')
